7d142a7bec532ff250597ca29e1801a46ee4b97a
Higor <higordamascenostella@gmail.com>
2026-08-12 00:24:42 -0300

    :bug: CSS copiado

--- blogpessoal--/vite.config.ts
+++ blogpessoal--/vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
- 
-// https://vite.dev/config/
+import { resolve } from 'path'
+
 export default defineConfig({
-  base: '/Portifolio_Frontend/',
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+    base: '/Portifolio_Frontend/',
+
+    plugins: [
+        react(),
+        babel({
+            presets: [reactCompilerPreset()]
+        })
+    ],
+
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                success: resolve(__dirname, 'success.html')
+            }
+        }
+    }
 })
